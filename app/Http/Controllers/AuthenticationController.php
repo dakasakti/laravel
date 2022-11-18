eb8cce3e68d7e0fbb0f4ad3e2c6549d4db040178
Mahmuda Karima <dakasakti.id@gmail.com>
2022-11-18 08:29:11 +0700

    update-prevent-back-history

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -14,6 +14,7 @@ class AuthenticationController extends Controller
 {
     public function __construct()
     {
+        $this->middleware("preventBackHistory");
         $this->middleware("guest", ["except" => "logout"]);
     }
 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        "not.verified" => \App\Http\Middleware\NotVerified::class,
+        'not.verified' => \App\Http\Middleware\NotVerified::class,
+        'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
     ];
 }
